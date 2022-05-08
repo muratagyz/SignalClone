a7a8611715e697091394cf3f9abae731ec75e811
Murat AĞYÜZ <mrt.agyzz.00@outlook.com>
2022-05-08 23:02:37 +0300

    8.05.2022 Project Create And Login

---
+++ SignalClone/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBu3BWBEb6nChESySie0Hr44HlamBJsM_E",
+    authDomain: "signalclone-29267.firebaseapp.com",
+    projectId: "signalclone-29267",
+    storageBucket: "signalclone-29267.appspot.com",
+    messagingSenderId: "112677950811",
+    appId: "1:112677950811:web:959c1a5cd1216d93c2f67e",
+    measurementId: "G-YYJ311PTEJ"
+  };
---
+++ SignalClone/screens/LoginScreen.js
@@ -0,0 +1,62 @@
+import { KeyboardAvoidingView, StyleSheet, View, Text } from "react-native";
+import React, { useState } from "react";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+
+      <Image
+        source={{
+          uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+
+      <Button title="Login" onPress={signIn} containerStyle={styles.button} />
+      <Button title="Register" type="outline" containerStyle={styles.button} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
