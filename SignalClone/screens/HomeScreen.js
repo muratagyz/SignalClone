b73312859a45cd4db2f47db9150b852988021fb2
Murat AĞYÜZ <mrt.agyzz.00@outlook.com>
2022-05-09 14:19:05 +0300

    9.05.2022 register and login finish

---
+++ SignalClone/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import { SafeAreaView, View, Text, ScrollView } from "react-native";
+import React from "react";
+import CustomListItem from "../components/CustomListItem";
+
+const HomeScreen = () => {
+  use
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <CustomListItem></CustomListItem>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
