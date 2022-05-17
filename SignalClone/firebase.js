4380a5c8a61848e8d8ad00665943fe861960cfff
Murat AĞYÜZ <mrt.agyzz.00@outlook.com>
2022-05-17 23:45:02 +0300

    17.05.2022

--- SignalClone/firebase.js
+++ SignalClone/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth"
 import firebase from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBu3BWBEb6nChESySie0Hr44HlamBJsM_E",
-  authDomain: "signalclone-29267.firebaseapp.com",
-  projectId: "signalclone-29267",
-  storageBucket: "signalclone-29267.appspot.com",
-  messagingSenderId: "112677950811",
-  appId: "1:112677950811:web:959c1a5cd1216d93c2f67e",
-  measurementId: "G-YYJ311PTEJ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 let app;
